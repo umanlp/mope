4c00641d0d8a72119ec42e38bfa4496d237eecf8
inrehbei <ines@informatik.uni-mannheim.de>
2023-04-26 16:44:36 +0200

    adding baseline files

---
+++ mope_baseline/src/helpers.py
@@ -0,0 +1,41 @@
+import torch
+import os
+
+
+
+### Data
+
+def load_input(data):
+    if torch.cuda.is_available():    
+        LongTensor = torch.cuda.LongTensor 
+    else:
+        LongTensor = torch.LongTensor
+        
+    seq_lengths = [len(i) for i in data['input_ids']]    
+    input_ids = LongTensor(data['input_ids'])
+    attention_masks = LongTensor(data['attention_mask'])
+    label_ids = LongTensor(data['labels'])
+    seq_lengths = LongTensor(seq_lengths)
+    #token_type_ids = LongTensor(data['token_type_ids'])
+    #print("ids", len(input_ids), "mask", len(attention_masks), "labels", len(label_ids), "seq len", seq_lengths.shape)    
+    return input_ids, attention_masks, label_ids, seq_lengths
+
+
+### Models
+
+def save_model(output_dir, model_info, model, tokenizer):
+    # Create output directory if needed
+    out_path = output_dir # + '/' + model_info
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    if not os.path.exists(output_dir + '/' + model_info):
+        os.makedirs(output_dir + '/' + model_info)
+
+    print("Saving model to %s" % out_path)
+
+    model_to_save = model.module if hasattr(model, 'module') else model  # Take care of distributed/parallel training
+    model_to_save.save_pretrained(out_path)
+    tokenizer.save_pretrained(out_path)
+
+
+
---
+++ mope_baseline/src/tagger.py
@@ -0,0 +1,75 @@
+import transformers
+import torch
+import torch.nn as nn
+from transformers import AutoModelForTokenClassification
+
+
+
+class BertTagger(torch.nn.Module): 
+
+    def __init__(self, model_name, num_labels):
+        super(BertTagger, self).__init__()
+        self.bert = AutoModelForTokenClassification.from_pretrained(model_name, num_labels=num_labels)
+
+    def forward(self, input_id, mask, label):
+        output = self.bert(input_ids=input_id, attention_mask=mask, labels=label, return_dict=False)
+        return output
+
+
+
+"""
+    def __init__(self, encoder, taskmodels_dict):
+        
+        Setting MultitaskModel up as a PretrainedModel allows us
+        to take better advantage of Trainer features
+        
+        super().__init__(transformers.PretrainedConfig())
+
+        self.encoder = encoder
+        self.taskmodels_dict = nn.ModuleDict(taskmodels_dict)
+
+    @classmethod
+    def create(cls, model_name, model_type_dict, model_config_dict):
+        
+        This creates a MultitaskModel using the model class and config objects
+        from single-task models. 
+
+        We do this by creating each single-task model, and having them share
+        the same encoder transformer.
+        
+        shared_encoder = None
+        taskmodels_dict = {}
+        for task_name, model_type in model_type_dict.items():
+            model = model_type.from_pretrained(
+                model_name,
+                config=model_config_dict[task_name],
+            )
+            if shared_encoder is None:
+                shared_encoder = getattr(model, cls.get_encoder_attr_name(model))
+            else:
+                setattr(model, cls.get_encoder_attr_name(model), shared_encoder)
+            taskmodels_dict[task_name] = model
+        return cls(encoder=shared_encoder, taskmodels_dict=taskmodels_dict)
+
+
+    @classmethod
+    def get_encoder_attr_name(cls, model):
+        
+        The encoder transformer is named differently in each model "architecture".
+        This method lets us get the name of the encoder attribute
+        
+        model_class_name = model.__class__.__name__
+        if model_class_name.startswith("Bert"):
+            return "bert"
+        elif model_class_name.startswith("Roberta"):
+            return "roberta"
+        elif model_class_name.startswith("Albert"):
+            return "albert"
+        elif model_class_name.startswith("XLMRoberta"):
+            return "roberta"
+        else:
+            raise KeyError(f"Add support for new model {model_class_name}")
+
+    def forward(self, task_name, **kwargs):
+        return self.taskmodels_dict[task_name](**kwargs)
+"""
